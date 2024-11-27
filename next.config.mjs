97f413f52e83f87cb7dee154577a89b607d0e9d3
RuanV-S <ruan_vieiradasilva@hotmail.com>
2024-11-27 18:31:24 -0300

    feat: :sparkles: Estilizar página inicial e home com SPA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "maps.googleapis.com",
+        port: "",
+        pathname: "/maps/api/staticmap**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
